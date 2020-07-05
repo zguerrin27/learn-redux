bcf328963ee9d1bb66f9a6f8b260f031541aba3c
Zachary Guerrin <zguerrin@apple.com>
2020-07-05 09:38:41 -0400

    adding to github

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,1 +1,29 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// import css
+import css from './styles/style.styl'
+
+// import components 
+import Main from './components/Main'
+import Photogrid from './components/Photogrid'
+import Single from './components/Single'
+
+// import react router dependencies 
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import store, { history } from './store'
+
+const router = (
+  <Provider store={store}>
+  <Router history={history}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={Photogrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
+
